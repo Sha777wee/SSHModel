419839377e2e31c7287104517c5c52dbd1608d01
Shawee <757221692@qq.com>
2018-07-21 16:07:59 +0800

    add sql

---
+++ sshmodel.sql
@@ -0,0 +1,22 @@
+﻿# Host: 127.0.0.1  (Version 5.7.20-log)
+# Date: 2018-07-21 16:06:40
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+INSERT INTO `user` VALUES (1,'admin','123456');
